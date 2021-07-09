eabc401b83acbffae0a6c940c139112d81a21ee6
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2021-07-09 15:38:33 +0200

    🎆 init!

---
+++ src/main/java/fr/zenika/maven/formation/show_version_number/App.java
@@ -0,0 +1,32 @@
+package fr.zenika.maven.formation.show_version_number;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+public class App {
+	private static final Logger logger = Logger.getLogger(App.class.getName());
+
+	public App() {
+	}
+
+	public static void main(String[] args) throws Throwable {
+		App app = new App();
+		app.run();
+	}
+
+	private void run() throws IOException {
+		logger.info(String.format("Version %s", getProperties().getProperty("version")));
+		logger.info(String.format("Project artifact id %s", getProperties().getProperty("artifactId")));
+	}
+
+	private Properties getProperties() throws IOException {
+		try (InputStream stream = getClass().getClassLoader().getResourceAsStream("version.properties")) {
+			Properties props = new Properties();
+			props.load(stream);
+			return props;
+		}
+	}
+
+}
